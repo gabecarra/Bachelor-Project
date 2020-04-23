5e290113e9ad5c35fe669d573e8f9ecf4ad43968
Gabriel H. Carraretto <gabriel.carraretto@gmail.com>
2020-04-23 03:12:58 +0200

    Created matlab script, and updated dataset info

---
+++ mpii_human_pose/annotations_converter.m
@@ -0,0 +1,17 @@
+clear;
+data = load('dataset_original.mat');
+category = data.RELEASE.act;
+file = data.RELEASE.annolist;
+SIZE = size(data.RELEASE.act,1);
+
+images = {};
+
+for i = 1 : SIZE
+    images(i).name = file(i).image.name;
+    images(i).cat_name = category(i).cat_name;
+    images(i).act_name = category(i).act_name;
+    images(i).act_id = category(i).act_id;
+end
+
+
+save dataset images
