fe9a1d0138dded0a67036a1301c655608891fdfd
Gabriel H. Carraretto <gabriel.carraretto@gmail.com>
2020-05-03 01:58:37 +0200

    Created a first pipeline version

--- src/pipeline.py
+++ src/pipeline.py
@@ -5,6 +5,7 @@ from sys import platform
 import argparse
 import time
 import progress.bar as progress_bar
+import utils.graph_parser as gp
 
 # Import OpenPose python wrapper
 try:
@@ -47,56 +48,78 @@ def parse_config_params(arg_size):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    try:
-        # default params
-        parser.add_argument('--image_dir')
-        parser.add_argument('--video')
-        parser.add_argument('--display', default='0')
-        args = parser.parse_known_args()
 
-        # Custom params
-        params = parse_config_params(len(args[1]))
-        params["model_folder"] = "../openpose/models/"
+    # default params
+    parser.add_argument('--image_dir')
+    parser.add_argument('--video')
+    parser.add_argument('--display', default='0')
+    parser.add_argument('--write_json')
+    args = parser.parse_known_args()
 
-        # Starting OpenPose
-        opWrapper = op.WrapperPython()
-        opWrapper.configure(params)
-        opWrapper.start()
+    # Custom params
+    params = parse_config_params(len(args[1]))
+    params["model_folder"] = "../openpose/models/"
 
-        # Read frames on directory
-        if args[0].image_dir is not None:
-            imagePaths = op.get_images_on_directory(args[0].image_dir)
-        elif args[0].video is not None:
-            # TODO: implement video recognition
-            pass
-        start = time.time()
+    # Starting OpenPose
+    opWrapper = op.WrapperPython()
+    opWrapper.configure(params)
+    opWrapper.start()
 
-        bar = progress_bar.IncrementalBar(
-            'Applying pose recognition: ',
-            max=len(imagePaths),
-            suffix='%(index)d/%(max)d'
-        )
+    # Read frames on directory
+    if args[0].video is not None and args[0].image_dir is None:
+        # TODO: implement video recognition
+        imagePaths = "test"
+    elif args[0].image_dir is not None and args[0].video is None:
+        imagePaths = op.get_images_on_directory(args[0].image_dir)
+    else:
+        raise IndexError('Usage: ' + os.path.basename(__file__) +
+                         ' --image_dir [path] or --video [path]')
+    start = time.time()
 
-        # Process and display images
-        for imagePath in imagePaths:
-            datum = op.Datum()
-            imageToProcess = cv2.imread(imagePath)
-            datum.cvInputData = imageToProcess
-            opWrapper.emplaceAndPop([datum])
-            bar.next()
+    bar = progress_bar.IncrementalBar(
+        'Applying OpenPose recognition: ',
+        max=len(imagePaths),
+        suffix='%(index)d/%(max)d'
+    )
+    bar.start()
 
-            # print("Body keypoints: \n" + str(datum.poseKeypoints))
+    # Process and display images
+    for imagePath in imagePaths:
+        datum = op.Datum()
+        imageToProcess = cv2.imread(imagePath)
+        datum.cvInputData = imageToProcess
+        opWrapper.emplaceAndPop([datum])
+        bar.next()
 
-            if args[0].display == '1':
-                cv2.imshow("OpenPose Image", datum.cvOutputData)
-                key = cv2.waitKey(15)
-                if key == 27:
-                    break
-        bar.finish()
-        end = time.time()
-        print("Pose recognition successfully finished. Total time: "
-              + str(end - start) + " seconds")
-    except Exception as e:
-        print(e)
-        sys.exit(-1)
+        if args[0].display == '1':
+            cv2.imshow("OpenPose Image", datum.cvOutputData)
+            key = cv2.waitKey(15)
+            if key == 27:
+                break
+
+        # Pose(default)
+        keypts_list = [datum.poseKeypoints]
+        node_type = ['pose']
+        # Face
+        if 'face' in params:
+            keypts_list.append(datum.faceKeypoints)
+            node_type.append('face')
+        # Hand
+        if 'hand' in params:
+            keypts_list.append(datum.handKeypoints[0])
+            keypts_list.append(datum.handKeypoints[1])
+            node_type.append('hand')
+        # Request to write the output as json
+        if args[0].write_json is not None and keypts_list[0].size > 1:
+            gp.points_to_json(keypts_list,
+                              args[0].write_json,
+                              imageToProcess.shape,
+                              node_type,
+                              os.path.splitext(imagePath)[0].split('/')[-1])
+
+    bar.finish()
+    end = time.time()
+    print("Task successfully finished. Total time: {:.2f}s"
+          .format(end - start))
+
 
