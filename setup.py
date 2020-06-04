667c474da1a0a7710bb2d36ef3ae9b6e0f353aa4
Gabriel H. Carraretto <gabriel.carraretto@gmail.com>
2020-06-04 17:41:38 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='GraphPipe',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/gabecarra/GraphPipe',
+    license='MIT',
+    author='Gabriel Henrique Carraretto',
+    author_email='carrag@usi.ch',
+    description='The following project consists in a python package that applies 2D multi-person pose estimation to images and videos, and parses the results into attributed graphs. The goal of this project is to make available an all-in-one tool to build datasets based on real time 2D multi-person detection, and is part of my bachelor project at USI.'
+)
